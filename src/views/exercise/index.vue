e780d130710f486aed4521332ccf9634877d83ac
JXDS18FUJT <347613781@qq.com>
2024-11-06 17:56:03 +0800

    做题界面制作中

--- src/views/exercise/index.vue
+++ src/views/exercise/index.vue
@@ -1,58 +1,88 @@
 <template>
 	<view class="w-1920 block">
+		<view class="w-full h-466 flex pt-30 pl-30 flex-col items-start flex-wrap justify-start">
+
+			<view class="flex flex-col relative w-162 h-39 border-1px border-solid border-[#D6D9DDFF]">
+				<view class="text-[#FE3B2BFF] text-16 absolute top-0 left-13 translate-[0%,-50%]">理论考试</view>
+				<view class="text-14 pt-10">第 01 考台</view>
+			</view>
+			<view class="flex flex-col relative mt-30 w-162 h-340 pt-23 border-1px border-solid border-[#D6D9DDFF]">
+				<view class="text-[#FE3B2BFF] text-16 absolute top-0 left-13 translate-[0%,-50%]">理论考试</view>
+				<view>
+					<img class="w-118 rd-[50%]"
+						src="https://thirdwx.qlogo.cn/mmopen/vi_32/POgEwh4mIHO4nibH0KlMECNjjGxQUq24ZEaGT4poC6icRiccVGKSyXwibcPq4BWmiaIGuG1icwxaQX6grC9VemZoJ8rg/132" />
+					<view class="pt-10 w-full flex flex-wrap text-left pl-18 pr-18 text-14">
+						<view class="w-full">考生姓名：林笑笑</view>
+						<view class="w-full">性 别：男</view>
+						<view class="w-full">考试科目：科目一理论</view>
+						<view class="w-full">考试车型：D/E/F</view>
+						<view class="w-full">考试原因：初次申请</view>
+					</view>
+				</view>
+
+			</view>
+			<view class="w-1320 h-466 relative border-1px border-solid border-[#D6D9DDFF]">
+				<view class="text-[#FE3B2BFF] text-16 absolute top-0 left-13 translate-[0%,-50%]">理论考试</view>
+			</view>
+		</view>
+
+
+
+
 		<view class="w-full block border-b-1px border-b-solid border-b-[#ff4400]">网格</view>
 		<view class="pb-20 pt-20 pl-20 pr-20 block text-left">
 			<view class="w-400px h-400px absolute z-99 flex flex-wrap items-start">
-				<view v-for="item in 100" :key="item" class="w-40px flex-0 h-40px text-center lh-40px  text-[#0A1A33FF]">
+				<view v-for="item in 100" :key="item"
+					class="w-40px flex-0 h-40px text-center lh-40px  text-[#0A1A33FF]">
 					{{item}}
 				</view>
 			</view>
-			
-			<canvas class="w-400px h-400px"  width="400" height="400" id="grid"></canvas>	
+
+			<canvas class="w-400px h-400px" width="400" height="400" id="grid"></canvas>
 		</view>
-	
+
 	</view>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from "vue"
-	onMounted(()=>{
+	import { onMounted } from "vue"
+	onMounted(() => {
 		let gridDom = document.getElementById("grid") as HTMLCanvasElement
 		let ctx = gridDom.getContext('2d')
 		const gridDomWidth = 400
 		const gridDomHeight = 400
-		
-	
-		
-		
+
+
+
+
 		for (let i = 0; i < 10; i++) {
 			//上方边框
-			ctx.moveTo(0,0.5+i*40)
-			ctx.lineTo(gridDomWidth,0.5+i*40)
+			ctx.moveTo(0, 0.5 + i * 40)
+			ctx.lineTo(gridDomWidth, 0.5 + i * 40)
 			ctx.strokeStyle = '#CDCEC9FF'
 			ctx.stroke()
-			
+
 			//下方边框
-			ctx.moveTo(0,gridDomHeight-0.5)
-			ctx.lineTo(gridDomWidth,gridDomHeight-0.5)
+			ctx.moveTo(0, gridDomHeight - 0.5)
+			ctx.lineTo(gridDomWidth, gridDomHeight - 0.5)
 			ctx.strokeStyle = '#CDCEC9FF'
 			ctx.stroke()
 		}
-		
+
 		for (let i = 0; i < 10; i++) {
 			//左边边框
-			ctx.moveTo(0.5+i*40,0)
-			ctx.lineTo(0.5+i*40,gridDomHeight)
+			ctx.moveTo(0.5 + i * 40, 0)
+			ctx.lineTo(0.5 + i * 40, gridDomHeight)
 			ctx.strokeStyle = '#CDCEC9FF'
 			ctx.stroke()
-			
+
 			//右边边框
-			ctx.moveTo(gridDomWidth-0.5,0)
-			ctx.lineTo(gridDomWidth-0.5,gridDomHeight)
+			ctx.moveTo(gridDomWidth - 0.5, 0)
+			ctx.lineTo(gridDomWidth - 0.5, gridDomHeight)
 			ctx.strokeStyle = '#CDCEC9FF'
 			ctx.stroke()
 		}
-		
+
 	})
 </script>
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,16 +1,29 @@
 <template>
 
 	<view class="mr-auto text-black text-26">
-		这里是login{{msg}}
+		<text>这里是login{{msg}}</text>
+		<view id="wxlogin" class=""></view>
 	</view>
 </template>
 
 <script setup name="login" lang="ts">
-	import { ref } from 'vue';
-
+	import { onMounted, ref } from 'vue';
+	onMounted(() => {
+		const wxloginDom = document.getElementById("wxlogin")
+		var obj = new WxLogin({
+			self_redirect: true,
+			id: "wxlogin",
+			appid: "wx37cfd0fd981447d9",
+			scope: "snsapi_login",
+			redirect_uri: encodeURI("https://ct.xmze.net/"),
+			state: "state",
+			style: "black",//二维码黑白风格
+			href: ""
+		});
+	})
 	const msg = ref('登录中')
 	defineOptions({
-		name:'login'
+		name: 'login'
 	})
 </script>
 
